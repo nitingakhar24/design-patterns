9d12bca65aef97731cfef00f7e3c248600c405bf
nitingakhar <nitingakhar@yahoo.com>
2023-12-31 18:29:39 -0500

    Singleton pattern - logic to break singleton pattern

--- src/singleton/Example.java
+++ src/singleton/Example.java
@@ -1,8 +1,10 @@
 package singleton;
 
+import java.lang.reflect.Constructor;
+
 public class Example {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+
         LazySnack snack1 = LazySnack.getSnackInstance();
         System.out.println(snack1.hashCode());
         LazySnack snack2 = LazySnack.getSnackInstance();
@@ -12,5 +14,16 @@ public class Example {
         System.out.println(eagerRefreshment1.hashCode());
         EagerRefreshment eagerRefreshment2 = EagerRefreshment.getRefreshmentInstance();
         System.out.println(eagerRefreshment2.hashCode());
+
+
+        // Logic to Break Singleton pattern
+
+        LazySnack lazySnack1 = LazySnack.getSnackInstance();
+        System.out.println(lazySnack1.hashCode());
+
+        Constructor<LazySnack> constructor = LazySnack.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        LazySnack lazySnack2 = constructor.newInstance();
+        System.out.println(lazySnack2.hashCode());
     }
 }
