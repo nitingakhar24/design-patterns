b9291af6c5f72625e953fb0a751fd0b0e694ad43
nitingakhar <nitingakhar@yahoo.com>
2023-12-31 18:14:13 -0500

    Singleton pattern - using synchronized block for object creation

--- src/singleton/LazySnack.java
+++ src/singleton/LazySnack.java
@@ -4,18 +4,26 @@ package singleton;
  * 1. Private constructor.
  * 2. Create object with the help of a static method.
  * 3. Create field to store the object as private and static.
+ * 4. Use synchronized block in the object creation part in case of multiple thread.
  */
+
 public class LazySnack {
     private static LazySnack snack;
+
     private LazySnack() {
 
     }
 
     // Lazy initialization method
     public static LazySnack getSnackInstance() {
-    // create object of this Snack class only once
-        if(snack == null) {
-            snack = new LazySnack();
+        // create object of this Snack class only once
+        if (snack == null) {
+            synchronized (LazySnack.class) {
+                if (snack == null) {
+                    snack = new LazySnack();
+                }
+
+            }
         }
         return snack;
     }
